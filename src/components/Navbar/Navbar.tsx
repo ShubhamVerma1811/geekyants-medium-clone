dfc3554f432dbf57eb0dad2cff91423a274b85ba
Shubham <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-07-16 14:29:48 +0530

    Added Navbar for basic navigation

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/post/pid'>Post</NavLink>
+        </li>
+        <li>
+          <NavLink to='/login'>Log In</NavLink>
+        </li>
+        <li>
+          <NavLink to='/signup'>Sign Up</NavLink>
+        </li>
+        <li>
+          <NavLink to='/topic/topic_name'>Topic</NavLink>
+        </li>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
