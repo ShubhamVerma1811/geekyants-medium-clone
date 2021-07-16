5fd0b9550894a2defd67907ce931ceeb24b16533
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-07-16 17:16:46 +0530

    MIsc

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
-import './index.css'
 import { persistor, store } from './store'
 import './tailwind.css'
 
