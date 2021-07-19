e248148efe6c183b58c75e57885bc16201d44ffa
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-07-19 14:42:45 +0530

    initialised firebase

--- src/configs/firebase/init.ts
+++ src/configs/firebase/init.ts
@@ -1,23 +1,17 @@
-import firebase from "firebase"
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
 
-import {
-  API_KEY,
-  APP_ID,
-  AUTH_DOMAIN,
-  MESSAGING_SENDER_ID,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-} from './keys';
+const firebaseConfig = {
+  apiKey: import.meta.env.SNOWPACK_PUBLIC_API_KEY,
+  authDomain: import.meta.env.SNOWPACK_PUBLIC_AUTH_DOMAIN,
+  projectId: import.meta.env.SNOWPACK_PUBLIC_PROJECT_ID,
+  storageBucket: import.meta.env.SNOWPACK_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.SNOWPACK_PUBLIC_MESSAGING_SENDER_ID,
+  appId: import.meta.env.SNOWPACK_PUBLIC_APP_ID,
+}
+// firebase.js
 
- const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-};
+firebase.initializeApp(firebaseConfig)
 
-const app = !firebase.apps.length && firebase.initializeApp(firebaseConfig);
-
-export default app;
+export default firebase
